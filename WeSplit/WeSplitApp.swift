d99d0b6aed500160d3f57f31e702b017a796e0ed
suhashollakc <suhashollakc@gmail.com>
2025-06-05 19:16:39 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Suhas Holla Karkada Chandrashekar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
